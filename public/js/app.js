d6520b742b57dea0370c4281179b9cd1c7c7c4cb
Lauren Arfaras <laurenarfaras@Laurens-MacBook-Pro.local>
2018-02-06 02:15:28 -0500

    simplified language logic and added Hindi as a language

--- public/js/app.js
+++ public/js/app.js
@@ -13,36 +13,13 @@ app.controller('mainController',['$scope', '$http', function($scope, $http){
     console.log('translating');
     console.log('input = ' + $scope.input);
     console.log('output = ' + $scope.output);
-    var tar = 'en';
-    var src = 'en';
-    switch($scope.output){
-      case 'English':
-        tar = 'en';
-        break;
-      case 'Spanish':
-        tar = 'es';
-        break;
-      default:
-        tar = 'en';
-        break;
-    }
-    switch($scope.input){
-      case 'English':
-        src = 'en';
-        break;
-      case 'Spanish':
-        src = 'es';
-        break;
-      default:
-        src = 'en';
-        break;
-    }
-    $http.post(`https://translation.googleapis.com/language/translate/v2?q=${$scope.message}&target=${tar}&source=${src}&key=AIzaSyArCyLfgKWuKNVzm1b7FSZUjjAHDMySTXE`)
+
+    $http.post(`https://translation.googleapis.com/language/translate/v2?q=${$scope.message}&target=${$scope.output}&source=${$scope.input}&key=AIzaSyArCyLfgKWuKNVzm1b7FSZUjjAHDMySTXE`)
         .then(function(response) {
           console.log(response.data.data.translations[0].translatedText);
           $scope.messages.push(response.data.data.translations[0].translatedText);
         })
-    // $scope.send();
+
   }
 
   $scope.clear = function() {
