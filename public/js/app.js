5e0db7f8280c1c0e624b278b48869a6a41bcbff8
Lauren Arfaras <laurenarfaras@Laurens-MacBook-Pro.local>
2018-02-06 02:20:18 -0500

    cleared textarea once message sent

--- public/js/app.js
+++ public/js/app.js
@@ -4,22 +4,15 @@ app.controller('mainController',['$scope', '$http', function($scope, $http){
 
   $scope.messages = [];
 
-  $scope.send = function() {
-    $scope.messages.push($scope.message);
-    $scope.message = '';
-  }
-
   $scope.translate = function() {
-    console.log('translating');
-    console.log('input = ' + $scope.input);
-    console.log('output = ' + $scope.output);
 
     $http.post(`https://translation.googleapis.com/language/translate/v2?q=${$scope.message}&target=${$scope.output}&source=${$scope.input}&key=AIzaSyArCyLfgKWuKNVzm1b7FSZUjjAHDMySTXE`)
         .then(function(response) {
           console.log(response.data.data.translations[0].translatedText);
           $scope.messages.push(response.data.data.translations[0].translatedText);
-        })
+        });
 
+    $scope.message = '';
   }
 
   $scope.clear = function() {
